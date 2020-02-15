8d4203cd1e35eff1a0da4a878692f933b83f50fd
YakuI7 <52265864+YakuI7@users.noreply.github.com>
2020-02-15 14:11:14 -0500

    Bills rendered on homepage

--- app/blueprints/main/routes.py
+++ app/blueprints/main/routes.py
@@ -2,12 +2,18 @@ from flask import render_template, flash, redirect, url_for
 from app import db
 from app.models import User, Bill
 from flask_login import login_required, current_user
-
 from app.blueprints.main import main
 
 @main.route('/', methods=['GET','POST'])
 @login_required
 def index():
-    context = {      
+    id = current_user.id
+    #test = Bill.objects(user_id=id).order_by('due_date').all()
+    test = Bill.objects().order_by('due_date').all()
+    print(test)
+    
+    context = {
+        'bills': Bill.objects().order_by('due_date').all()
+        #'bills': Bill.objects(user_id=id)      
     }
     return render_template('index.html', **context)
